bb0f2ef2ca8954b6c7ffaa3fc84512960c338d27
wjsheves <code_5@icloud.com>
2024-04-07 09:32:37 -0400

    Initial commit

---
+++ src/main/java/net/hacker/stuffmod/StuffModClient.java
@@ -0,0 +1,10 @@
+package net.hacker.stuffmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StuffModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
